cc39223d2a72fd163cfd68b3dce1227f44a0b6a3
Diego Vaca <diegovacapaz@hotmail.com>
2023-03-08 13:31:20 -0300

    Se completo el punto 9

---
+++ js/punto9.js
@@ -0,0 +1,19 @@
+let divisor = 1;
+for(let i = 1; i<=500; i++){
+    document.write(i);
+    if(i%4 === 0 && i%9 === 0){
+        document.write(" (múltiplo de 4 y 9)");
+    }
+    else if(i%4===0){
+        document.write(" (múltiplo de 4)");
+    }
+    else if(i%9===0){
+        document.write(" (múltiplo de 9)");
+    }
+    document.write("<br>");
+    if(divisor%5 === 0){
+        document.write("--------------------------------------------------");
+        document.write("<br>");
+    }
+    divisor++;
+}
